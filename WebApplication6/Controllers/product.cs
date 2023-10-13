bae11d36884870d3e57d54deefc1473cd411077e
Haider Ali <Haider Ali@DESKTOP-L3IJC2A>
2023-10-13 15:45:39 -0400

    created product controller and called the controller

---
+++ WebApplication6/Controllers/product.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication6.Controllers
+{
+    public class product : Controller
+    {
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
